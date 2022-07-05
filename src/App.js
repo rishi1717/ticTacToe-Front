9bc49da643497b7974d5d7d808555908e7e3448f
rishi1717 <rshrrrss@gmail.com>
2022-07-05 19:15:13 +0530

    app.js cleanup

--- src/App.js
+++ src/App.js
@@ -16,58 +16,39 @@ import FriendList from "./pages/friendList"
 import { LevelProvider } from "./contextApi/levelContext"
 import { ThemeContextProvider } from "./contextApi/themeContext"
 import Leaderboard from "./pages/leaderboard"
+import { light, dark, levels } from "./utils"
+
 dotenv.config()
-
-const light = {
-	palette: {
-		mode: "light",
-	},
-}
-
-const dark = {
-	palette: {
-		mode: "dark",
-	},
-}
 
 function App() {
 	const dispatch = useDispatch()
 	const user = useSelector((state) => state.user.user)
-
-	const levels = {
-		0: "Novice",
-		1: "Apprentice",
-		2: "Intermediate",
-		3: "Expert",
-		4: "Master",
-		5: "Grandmaster",
-	}
-
 	const [darkTheme, setDarkTheme] = React.useState(true)
 
-	useEffect(() => {
-		;(async () => {
-			const socket = io(process.env.REACT_APP_SERVER)
-			if (!user._id) {
-				try {
-					const res = await axios.post("/guests", {})
-					socket.emit("connection", { _id: res.data.guest._id })
-					dispatch(setUser(res.data.guest))
-				} catch (e) {
-					console.log(e)
-				}
-			} else {
-				socket.emit("connection", { _id: user._id })
+	const connectUser = async () => {
+		const socket = io(process.env.REACT_APP_SERVER)
+		if (!user._id) {
+			try {
+				const res = await axios.post("/guests", {})
+				socket.emit("connection", { _id: res.data.guest._id })
+				dispatch(setUser(res.data.guest))
+			} catch (e) {
+				console.log(e)
 			}
-		})()
+		} else {
+			socket.emit("connection", { _id: user._id })
+		}
+	}
+
+	useEffect(() => {
+		connectUser()
 	}, [user])
+
 	return (
 		<LevelProvider value={levels}>
 			<ThemeContextProvider value={[darkTheme, setDarkTheme]}>
 				<ThemeProvider
-					theme={
-						darkTheme ? createTheme(dark) : createTheme(light)
-					}
+					theme={darkTheme ? createTheme(dark) : createTheme(light)}
 				>
 					<CssBaseline />
 					<BrowserRouter>
---
+++ src/utils.js
@@ -0,0 +1,20 @@
+export const dark = {
+	palette: {
+		mode: "dark",
+	},
+}
+
+export const light = {
+	palette: {
+		mode: "light",
+	},
+}
+
+export const levels = {
+	0: "Novice",
+	1: "Apprentice",
+	2: "Intermediate",
+	3: "Expert",
+	4: "Master",
+	5: "Grandmaster",
+}
